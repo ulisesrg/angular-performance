f2506142969d0b982f833e06f2dac57b1c50144f
Ulises Rojas González <ulises.rogo@gmail.com>
2020-12-10 08:49:57 -0600

    Add a custom preloading strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 import { LayoutComponent } from './layout/layout.component';
-
+import { PreloadService } from './core/services/preload/preload.service';
 import { AdminGuard } from './admin.guard';
 
 const routes: Routes = [
@@ -17,45 +17,58 @@ const routes: Routes = [
       },
       {
         path: 'home',
-        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
+        loadChildren: () =>
+          import('./home/home.module').then((m) => m.HomeModule),
+        data: { preload: true },
       },
       {
         path: 'products',
-        loadChildren: () => import('./product/product.module').then(m => m.ProductModule)
+        loadChildren: () =>
+          import('./product/product.module').then((m) => m.ProductModule),
+        data: { preload: true },
       },
       {
         path: 'contact',
-        loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)
+        loadChildren: () =>
+          import('./contact/contact.module').then((m) => m.ContactModule),
       },
       {
         path: 'order',
-        loadChildren: () => import('./order/order.module').then(m => m.OrderModule)
+        loadChildren: () =>
+          import('./order/order.module').then((m) => m.OrderModule),
       },
       {
         path: 'demo',
-        loadChildren: () => import('./demo/demo.module').then(m => m.DemoModule)
+        loadChildren: () =>
+          import('./demo/demo.module').then((m) => m.DemoModule),
       },
-    ]
+    ],
   },
   {
     path: 'admin',
     canActivate: [AdminGuard],
-    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule)
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
   },
   {
     path: 'auth',
-    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
   },
   {
     path: '**',
-    loadChildren: () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule)
+    loadChildren: () =>
+      import('./page-not-found/page-not-found.module').then(
+        (m) => m.PageNotFoundModule
+      ),
   },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, {
-    preloadingStrategy: PreloadAllModules
-  })],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes, {
+      preloadingStrategy: PreloadService,
+    }),
+  ],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
