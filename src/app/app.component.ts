4627ce04a48ecdc0c74c70ab5c416791804df84f
Ulises Rojas González <ulises.rogo@gmail.com>
2020-12-16 19:50:53 -0600

    Implement Google Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
+declare var gtag;
 
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  constructor(private router: Router) {
+    const navEndEvents$ = this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    );
+
+    navEndEvents$.subscribe((event: NavigationEnd) => {
+      gtag('config', 'G-T68RBYNPXD', {
+        page_path: event.urlAfterRedirects,
+      });
+    });
+  }
 }
