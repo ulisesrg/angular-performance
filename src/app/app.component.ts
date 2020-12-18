55ada9aad6503edd601cc3bb856981dcffe2cac8
Ulises Rojas González <ulises.rogo@gmail.com>
2020-12-18 14:01:12 -0600

    Implement SSR with Universal

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, PLATFORM_ID, Inject } from '@angular/core';
 import { NavigationEnd, Router } from '@angular/router';
 import { filter } from 'rxjs/operators';
+import { isPlatformBrowser, isPlatformServer } from '@angular/common';
 
 declare var gtag;
 
@@ -10,15 +11,20 @@ declare var gtag;
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  constructor(private router: Router) {
-    const navEndEvents$ = this.router.events.pipe(
-      filter((event) => event instanceof NavigationEnd)
-    );
-
-    navEndEvents$.subscribe((event: NavigationEnd) => {
-      gtag('config', 'G-T68RBYNPXD', {
-        page_path: event.urlAfterRedirects,
+  constructor(
+    private router: Router,
+    // tslint:disable-next-line: ban-types
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      const navEndEvents$ = this.router.events.pipe(
+        filter((event) => event instanceof NavigationEnd)
+      );
+      navEndEvents$.subscribe((event: NavigationEnd) => {
+        gtag('config', 'G-T68RBYNPXD', {
+          page_path: event.urlAfterRedirects,
+        });
       });
-    });
+    }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -24,7 +24,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     LayoutComponent,
   ],
   imports: [
-    BrowserModule,
+    BrowserModule.withServerTransition({ appId: 'serverApp' }),
     AppRoutingModule,
     FormsModule,
     SharedModule,
