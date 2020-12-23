6c843b8c9caa7175567b863ecfeaf6db2dbaf800
Ulises Rojas González <ulises.rogo@gmail.com>
2020-12-23 09:07:35 -0600

    Implement ChangeDetection: OnPush for better performance

---
+++ src/app/contact/components/list/list.component.ts
@@ -0,0 +1,46 @@
+import {
+  Component,
+  OnInit,
+  Input,
+  EventEmitter,
+  Output,
+  ChangeDetectionStrategy,
+} from '@angular/core';
+import { EmployeeData } from '../../../core/models/employee.model';
+
+const fibonacci = (num: number): number => {
+  if (num === 1 || num === 2) {
+    return 1;
+  }
+  return fibonacci(num - 1) + fibonacci(num - 2);
+};
+
+@Component({
+  selector: 'app-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class ListComponent implements OnInit {
+  @Input() title: string;
+  @Input() data: EmployeeData[] = [];
+
+  @Output() add = new EventEmitter<string>();
+
+  label: string;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  addItem() {
+    this.add.emit(this.label);
+    this.label = '';
+  }
+
+  calc(item: EmployeeData) {
+    // console.log('calc', item.num);
+    console.log('list', this.title);
+    return fibonacci(item.num);
+  }
+}
