66e8a5e333f68481fd862c47594c02bc8749f8b1
Ulises Rojas González <ulises.rogo@gmail.com>
2020-12-23 11:34:21 -0600

    Implement async pipe to manage the unsubscription

--- src/app/contact/components/layout/layout.component.ts
+++ src/app/contact/components/layout/layout.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { GeneratorService } from '../../../core/services/generator.service';
 import { EmployeeData } from '../../../core/models/employee.model';
-import { Subscription } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 const names = ['thomas', 'alfredo', 'tony', 'jeff'];
 
@@ -13,23 +14,27 @@ const names = ['thomas', 'alfredo', 'tony', 'jeff'];
 export class LayoutComponent implements OnInit, OnDestroy {
   salesList: EmployeeData[] = [];
   bList: EmployeeData[] = [];
-  value: number;
-  sub$: Subscription;
+  value$: Observable<number>;
+  // sub$: Subscription;
 
-  constructor(private generatorService: GeneratorService) {}
+  constructor(private generatorService: GeneratorService) {
+    this.value$ = this.generatorService
+      .getData()
+      .pipe(tap((num) => console.log(num)));
+  }
 
   ngOnInit() {
     this.salesList = this.generatorService.generate(names, [10, 20], 10);
     this.bList = this.generatorService.generate(names, [10, 20], 10);
-    this.sub$ = this.generatorService.getData().subscribe((value) => {
-      this.value = value;
-      console.log(this.value);
-    });
+    // this.sub$ = this.generatorService.getData().subscribe((value) => {
+    //   this.value = value;
+    //   console.log(this.value);
+    // });
   }
 
   ngOnDestroy() {
-    console.log('destroy');
-    this.sub$.unsubscribe();
+    // console.log('destroy');
+    // this.sub$.unsubscribe();
   }
 
   addItem(list: EmployeeData[], label: string) {
